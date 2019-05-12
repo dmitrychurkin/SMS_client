6171403d31827a4e7fd29fd75567ef7ce8c71a12
dmitry <dmitrychurkin1988@gmail.com>
2019-05-12 23:18:50 +0300

    display device full info

--- App.js
+++ App.js
@@ -22,12 +22,78 @@ export default class App extends Component {
 
   state = {
     socketId: null,
-    appName: DeviceInfo.getApplicationName()
+    appName: DeviceInfo.getApplicationName(),
+    apiLevel: DeviceInfo.getAPILevel(),
+    batteryLevel: null,
+    brand: DeviceInfo.getBrand(),
+    buildNumber: DeviceInfo.getBuildNumber(),
+    bundleId: DeviceInfo.getBundleId(),
+    carrier: DeviceInfo.getCarrier(),
+    deviceCountry: DeviceInfo.getDeviceCountry(),
+    deviceId: DeviceInfo.getDeviceId(),
+    deviceLocale: DeviceInfo.getDeviceLocale(),
+    preferredLocales: DeviceInfo.getPreferredLocales(),
+    deviceName: DeviceInfo.getDeviceName(),
+    firstInstallTime: new Date(DeviceInfo.getFirstInstallTime()),
+    fontScale: DeviceInfo.getFontScale(),
+    freeDiskStorage: DeviceInfo.getFreeDiskStorage(),
+    ip: null,
+    installReferrer: DeviceInfo.getInstallReferrer(),
+    instanceId: DeviceInfo.getInstanceID(),
+    lastUpdateTime: new Date(DeviceInfo.getLastUpdateTime()),
+    mac: null,
+    manufacturer: DeviceInfo.getManufacturer(),
+    maxMemory: DeviceInfo.getMaxMemory(),
+    model: DeviceInfo.getModel(),
+    phoneNumber: DeviceInfo.getPhoneNumber(),
+    readableVersion: DeviceInfo.getReadableVersion(),
+    serialNumber: DeviceInfo.getSerialNumber(),
+    systemName: DeviceInfo.getSystemName(),
+    systemVersion: DeviceInfo.getSystemVersion(),
+    osBuildId: DeviceInfo.getBuildId(),
+    timezone: DeviceInfo.getTimezone(),
+    storageSize: DeviceInfo.getTotalDiskCapacity(),
+    totalMemory: DeviceInfo.getTotalMemory(),
+    uniqueId: DeviceInfo.getUniqueID(),
+    userAgent: DeviceInfo.getUserAgent(),
+    version: DeviceInfo.getVersion(),
+    is24Hour: DeviceInfo.is24Hour(),
+    airPlaneModeOn: null,
+    isCharging: null,
+    isEmulator: DeviceInfo.isEmulator(),
+    isPinOrFingerprintSet: null,
+    isTablet: DeviceInfo.isTablet(),
+    isLandscape: DeviceInfo.isLandscape(),
+    hasNotch: DeviceInfo.hasNotch(),
+    deviceType: DeviceInfo.getDeviceType(),
+    isAutoDateAndTime: null,
+    isAutoTimeZone: null,
+    supportedABIs: DeviceInfo.supportedABIs(),
+    features: null
   };
 
   socket = null;
 
   componentDidMount() {
+    DeviceInfo.getBatteryLevel()
+      .then(batteryLevel => this.setState({ batteryLevel }));
+    DeviceInfo.getIPAddress()
+      .then(ip => this.setState({ ip }));
+    DeviceInfo.getMACAddress()
+      .then(mac => this.setState({ mac }));
+    DeviceInfo.isAirPlaneMode()
+      .then(airPlaneModeOn => this.setState({ airPlaneModeOn }));
+    DeviceInfo.isBatteryCharging()
+      .then(isCharging => this.setState({ isCharging }));
+    DeviceInfo.isPinOrFingerprintSet()(isPinOrFingerprintSet => this.setState({ isPinOrFingerprintSet }));
+    DeviceInfo.isAutoDateAndTime()
+      .then(isAutoDateAndTime => this.setState({ isAutoDateAndTime }));
+    DeviceInfo.isAutoTimeZone()
+      .then(isAutoTimeZone => this.setState({ isAutoTimeZone }));
+    DeviceInfo.getSystemAvailableFeatures()
+      .then(features => this.setState({ features }));
+
+
     const socket = SocketIOClient('wss://sms-service-spdev.herokuapp.com');
 
     socket.on('error', err => Alert.alert(
@@ -59,7 +125,7 @@ export default class App extends Component {
 
   render() {
     return (
-      <ScrollView contentContainerStyle={styles.container}>
+      <ScrollView style={{ flex: 1 }} contentContainerStyle={styles.container}>
         <Text style={styles.welcome}>Device info:</Text>
         {
           Object.entries(this.state).map(([key, value]) => (
@@ -73,7 +139,6 @@ export default class App extends Component {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
